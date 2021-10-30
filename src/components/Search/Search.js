35ffde18694f0c8fe3ffe403247c3cc440864a38
Kyle McKell <46072760+KyleMcKell@users.noreply.github.com>
2021-10-30 14:57:47 -0600

    finished superheader

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,7 +1,56 @@
 import React from 'react';
+import styled from 'styled-components';
+
+import Icon from '../Icon';
+
+import { COLORS } from '../../constants';
 
 const Search = () => {
-	return <div>Hello I am a searchbars</div>;
+	return (
+		<Wrapper>
+			<IconWrapper>
+				<Icon id={'search'} size={16} strokeWidth={2} />
+			</IconWrapper>
+			<TextInput placeholder={'Search...'} />
+		</Wrapper>
+	);
 };
+
+const Wrapper = styled.label`
+	color: inherit;
+	position: relative;
+`;
+
+const TextInput = styled.input`
+	max-width: 175px;
+	background: none;
+	border: none;
+	border-bottom: 1px solid ${COLORS.gray[300]};
+	height: 28px;
+	padding-left: 28px;
+	font-size: inherit;
+	color: inherit;
+
+	&:focus {
+		outline: 2px solid black;
+		outline: 5px auto -webkit-focus-ring-color;
+	}
+
+	&::placeholder {
+		font-weight: 400;
+		color: ${COLORS.gray[500]};
+	}
+`;
+
+const IconWrapper = styled.div`
+	width: 16px;
+	height: 16px;
+	position: absolute;
+	top: 0;
+	bottom: 0;
+	left: 0;
+	margin: auto 0;
+	pointer-events: none;
+`;
 
 export default Search;
--- src/components/SuperHeader/SuperHeader.js
+++ src/components/SuperHeader/SuperHeader.js
@@ -2,31 +2,37 @@ import React from 'react';
 import styled from 'styled-components';
 
 import Icon from '../Icon';
+import Search from '../Search';
 
 import { COLORS, WEIGHTS } from '../../constants';
-import Search from '../Search';
 
 const SuperHeader = () => {
 	return (
-		<Container>
+		<Wrapper>
 			<Blurb>Free shipping on domenstic orders over $75!</Blurb>
 			<RightContainer>
 				<Search />
 				<HelpLink href="/">Help</HelpLink>
 				<IconWrapper href="/">
-					<Icon strokeWidth={1} id={'shopping-bag'} />
+					<Icon
+						strokeWidth={1}
+						id={'shopping-bag'}
+						size={24}
+						color={COLORS.white}
+					/>
 				</IconWrapper>
 			</RightContainer>
-		</Container>
+		</Wrapper>
 	);
 };
 
-const Container = styled.div`
+const Wrapper = styled.div`
 	background-color: ${COLORS.gray[900]};
 	display: flex;
 	justify-content: space-between;
-	align-items: center;
+	align-items: baseline;
 	padding: 12px 32px;
+	font-size: ${14 / 16} px;
 `;
 
 const Blurb = styled.h3`
@@ -39,6 +45,7 @@ const RightContainer = styled.div`
 	justify-content: space-between;
 	min-width: 300px;
 	color: ${COLORS.gray[300]};
+	align-items: baseline;
 `;
 
 const HelpLink = styled.a`
@@ -47,9 +54,15 @@ const HelpLink = styled.a`
 `;
 
 const IconWrapper = styled.a`
-	height: 20px;
-	width: 20px;
+	height: 24px;
+	width: 24px;
 	color: inherit;
+	pointer-events: none;
+	align-self: center;
+
+	&:hover {
+		color: ${COLORS.white};
+	}
 `;
 
 export default SuperHeader;
