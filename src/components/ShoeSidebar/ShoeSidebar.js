f6748b49d43639464049453b32fa4f52148ba484
Kyle McKell <46072760+KyleMcKell@users.noreply.github.com>
2021-11-02 11:32:24 -0600

    created sidebar

--- src/components/ShoeSidebar/ShoeSidebar.js
+++ src/components/ShoeSidebar/ShoeSidebar.js
@@ -1,11 +1,52 @@
 import React from 'react';
+import styled from 'styled-components';
+
+import { COLORS, WEIGHTS } from '../../constants';
 
 const ShoeSidebar = () => {
 	return (
-		<div>
-			<h2>i am a sidebar</h2>
-		</div>
+		<Wrapper>
+			<Link href="/lifestyle">Lifestyle</Link>
+			<Link href="/jordan">Jordan</Link>
+			<ActiveLink href="/running">Running</ActiveLink>
+			<Link href="/basketball">Basketball</Link>
+			<Link href="/training">Training &amp; Gym</Link>
+			<Link href="/football">Football</Link>
+			<Link href="/skateboarding">Skateboarding</Link>
+			<Link href="/us-football">American Football</Link>
+			<Link href="/baseball">Baseball</Link>
+			<Link href="/golf">Golf</Link>
+			<Link href="/tennis">Tennis</Link>
+			<Link href="/athletics">Athletics</Link>
+			<Link href="/walking">Walking</Link>
+		</Wrapper>
 	);
 };
+
+const Wrapper = styled.aside`
+	display: flex;
+	flex-direction: column;
+`;
+
+const Link = styled.a`
+	text-decoration: none;
+	color: ${COLORS.gray[900]};
+	font-weight: ${WEIGHTS.medium};
+	display: block;
+	line-height: 2;
+
+	&:hover {
+		color: ${COLORS.gray[700]};
+	}
+`;
+
+const ActiveLink = styled(Link)`
+	color: ${COLORS.primary};
+
+	&:hover {
+		color: ${COLORS.primary};
+		opacity: 80%;
+	}
+`;
 
 export default ShoeSidebar;
