e0d4541d2105266d7ce803a1ed02e9ce076ac9d5
Kyle McKell <46072760+KyleMcKell@users.noreply.github.com>
2021-11-02 10:37:40 -0600

    made layout of main and left column

--- src/components/ShoeIndex/ShoeIndex.js
+++ src/components/ShoeIndex/ShoeIndex.js
@@ -4,18 +4,27 @@ import styled from 'styled-components';
 const ShoeIndex = () => {
 	return (
 		<Wrapper>
-			<LeftColumn />
 			<MainColumn>
-				<h1>Shoe Index</h1>
+				<h2>Middle</h2>
 			</MainColumn>
+			<LeftColumn>
+				<h2>Left</h2>
+			</LeftColumn>
 		</Wrapper>
 	);
 };
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+	display: flex;
+	flex-direction: row-reverse;
+`;
 
-const LeftColumn = styled.div``;
+const LeftColumn = styled.div`
+	width: 280px;
+`;
 
-const MainColumn = styled.div``;
+const MainColumn = styled.div`
+	flex: 1;
+`;
 
 export default ShoeIndex;
